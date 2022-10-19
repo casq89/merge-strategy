e9c31f2c1bff2e0675835171806baa861ad7b857
Camillo Sanchez <casq89@gmail.com>
2022-10-19 12:23:31 -0500

    Second greet from A

--- file-a.js
+++ file-a.js
@@ -1,3 +1,5 @@
-console.log("Hi from file A");
+console.log("Hi from feature A");
 console.log("how are you from file A");
 console.log("Hi from feature B");
+
+console.log("Second greet from featrure A");
